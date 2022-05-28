6183b767b74e87b7468cc5c2af45b096572c2b91
RonPc <ronf07f@gmail.com>
2022-05-28 12:56:43 +0300

    start code clean up

--- app/src/main/java/com/example/jobmatch/GetMoreInfoActivity.java
+++ app/src/main/java/com/example/jobmatch/GetMoreInfoActivity.java
@@ -4,11 +4,8 @@ import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ContentValues;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -21,42 +18,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-
-import com.bumptech.glide.Glide;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
-
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class GetMoreInfoActivity extends AppCompatActivity {
-    private EditText nameField, phoneField,xpField,positionField;
-    private Button backButton,confirmButton;
+    private EditText nameField, phoneField,xpField;
+    private Button confirmButton;
     private ImageView profileImage;
-    Uri cam_uri;
-
-
-
     private final FirebaseAuth mAuth = FirebaseAuth.getInstance();
     private final FirebaseFirestore DB = FirebaseFirestore.getInstance();
-
     private DocumentReference userDB;
-
     private String userId, name, phone, profileImageUrl,userType,xp;
-
-    private Uri resultUri;
+    private Uri resultUri ,cam_uri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,7 +52,6 @@ public class GetMoreInfoActivity extends AppCompatActivity {
         nameField =  findViewById(R.id.name);
         phoneField = findViewById(R.id.phone);
         xpField = findViewById(R.id.xp);
-      //  backButton = findViewById(R.id.back);
         confirmButton = findViewById(R.id.confirm);
         profileImage = findViewById(R.id.profilePic);
         userType = getIntent().getStringExtra(GlobalVerbs.USER_TYPE);
@@ -98,38 +79,14 @@ public class GetMoreInfoActivity extends AppCompatActivity {
     }
 
     public void listeners(){
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
+        confirmButton.setOnClickListener(v ->  {
                 if(nameField!=null&&phoneField!=null)
                     saveUserInfo();
                     Intent intent = new Intent(GetMoreInfoActivity.this,MainActivity.class);
                     startActivity(intent);
-
-
-            }
         });
-
-        profileImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Intent intent = new Intent(Intent.ACTION_PICK);
-                // intent.setType("image/*");
-                //startGallery.launch("image/*");
-                chooseProfilePicture();
-
-            }
-        });
-/*
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                return;
-            }
-        });*/
-
+        profileImage.setOnClickListener(v ->
+            chooseProfilePicture());
     }
 
     private void chooseProfilePicture() {
@@ -138,13 +95,10 @@ public class GetMoreInfoActivity extends AppCompatActivity {
         View dialogView = inflater.inflate(R.layout.chose_profile_pic_dialog,null);
         builder.setCancelable(false);
         builder.setView(dialogView);
-
         AlertDialog alertDialogProfilePic =builder.create();
         alertDialogProfilePic.show();
-
         ImageView Camera = dialogView.findViewById(R.id.choseCamera);
         ImageView Gallery = dialogView.findViewById(R.id.choseGallery);
-
         Camera.setOnClickListener(v -> {
             //open camera
             openCamera();
@@ -164,14 +118,8 @@ public class GetMoreInfoActivity extends AppCompatActivity {
             cam_uri = GetMoreInfoActivity.this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
             Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, cam_uri);
-
-            //startActivityForResult(cameraIntent, IMAGE_PICK_CAMERA_CODE); // OLD WAY
-            startCamera.launch(cameraIntent);                // VERY NEW WAY
-
-
+            startCamera.launch(cameraIntent);
         }
-
-
 
     ActivityResultLauncher<Intent> startCamera = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -180,8 +128,7 @@ public class GetMoreInfoActivity extends AppCompatActivity {
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == RESULT_OK) {
                         // There are no request codes
-                        final Uri imageUri = cam_uri;
-                        resultUri = imageUri;
+                        resultUri = cam_uri;
                         profileImage.setImageURI(resultUri);
                         if (resultUri != null) {
                             StorageReference filepath = FirebaseStorage.getInstance().getReference().child("profileImages").child(userId);
@@ -196,46 +143,28 @@ public class GetMoreInfoActivity extends AppCompatActivity {
                             bitmap.compress(Bitmap.CompressFormat.JPEG,20,baos);
                             byte[] pic =  baos.toByteArray();
                             UploadTask uploadTask = filepath.putBytes(pic);
-                            uploadTask.addOnFailureListener(new OnFailureListener() {
-                                @Override
-                                public void onFailure(@NonNull Exception e) {
-                                    Log.i("banana",e.toString());
-                                }
-                            });
-                            uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                                @Override
-                                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            uploadTask.addOnFailureListener(e -> Log.i(GlobalVerbs.TAG,e.toString()));
+                            uploadTask.addOnSuccessListener(taskSnapshot ->  {
+
                                     if(taskSnapshot.getMetadata() != null){
                                         if(taskSnapshot.getMetadata().getReference()!=null){
-                                            Task<Uri>  result = taskSnapshot.getStorage().getDownloadUrl();
-                                            result.addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                                @Override
-                                                public void onSuccess(Uri uri) {
-                                                    String imageUrl = uri.toString();
-                                                    profileImageUrl=imageUrl.toString();
-                                                    return;
-                                                }
-                                            });
+                                            Task<Uri>  resultUri = taskSnapshot.getStorage().getDownloadUrl();
+                                            resultUri.addOnSuccessListener(uri ->
+                                                    profileImageUrl=uri.toString());
                                         }
                                     }
-                                }
                             });
-
                         } else {
                             finish();
                         }
-
                     }
                 }
             });
 
-
     ActivityResultLauncher<String> startGallery = registerForActivityResult(new ActivityResultContracts.GetContent(), new ActivityResultCallback<Uri>() {
         @Override
         public void onActivityResult(Uri result) {
-
-            final Uri imageUri = result;
-            resultUri = imageUri;
+            resultUri = result;
             profileImage.setImageURI(resultUri);
             if (resultUri != null) {
                 StorageReference filepath = FirebaseStorage.getInstance().getReference().child("profileImages").child(userId);
@@ -249,43 +178,20 @@ public class GetMoreInfoActivity extends AppCompatActivity {
                 bitmap.compress(Bitmap.CompressFormat.JPEG,20,baos);
                 byte[] pic =  baos.toByteArray();
                 UploadTask uploadTask = filepath.putBytes(pic);
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.i("banana",e.toString());
-                    }
-                });
-                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                uploadTask.addOnFailureListener(e -> Log.i("banana",e.toString()));
+                uploadTask.addOnSuccessListener(taskSnapshot ->  {
+
                         if(taskSnapshot.getMetadata() != null){
                             if(taskSnapshot.getMetadata().getReference()!=null){
-                                Task<Uri> result = taskSnapshot.getStorage().getDownloadUrl();
-                                result.addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                    @Override
-                                    public void onSuccess(Uri uri) {
-                                        String imageUrl = uri.toString();
-                                        profileImageUrl=imageUrl.toString();
-
-                                        return;
-                                    }
-                                });
+                                Task<Uri> resultUri = taskSnapshot.getStorage().getDownloadUrl();
+                                resultUri.addOnSuccessListener(uri -> profileImageUrl=uri.toString());
                             }
                         }
-                        //String downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
-
-
-                    }
                 });
-
             } else {
                 finish();
             }
-
-
-
         }
-
     });
 
     private void saveUserInfo() {
@@ -296,58 +202,6 @@ public class GetMoreInfoActivity extends AppCompatActivity {
         Users user = new Users(name,phone,15,profileImageUrl,userType,xp);
         Map<String,Object> userInfo = new HashMap<>();
         userInfo.put("user",user);
-       // userInfo.put("name",name);
-        //userInfo.put("phone",phone);
-       // userInfo.put("user",user);
         userDB.set(userInfo);
     }
-    /*
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1 && resultCode == Activity.RESULT_OK) {
-            final Uri imageUri = data.getData();
-            resultUri = imageUri;
-            profileImage.setImageURI(resultUri);
-            if (resultUri != null) {
-                StorageReference filepath = FirebaseStorage.getInstance().getReference().child("profileImages").child(userId);
-                Bitmap bitmap = null;
-                try {
-                    bitmap = MediaStore.Images.Media.getBitmap(getApplication().getContentResolver(), resultUri);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                bitmap.compress(Bitmap.CompressFormat.JPEG,20,baos);
-                byte[] pic =  baos.toByteArray();
-                UploadTask uploadTask = filepath.putBytes(pic);
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.i("banana",e.toString());
-                    }
-                });
-                uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        Task<Uri> downloadUrl = taskSnapshot.getStorage().getDownloadUrl();
-
-                        Map<String,Object> userInfo = new HashMap<>();
-                        userInfo.put("profileImageUrl", downloadUrl.toString());
-                        userDB.update(userInfo);
-                        finish();
-                        return;
-                    }
-                });
-
-            } else {
-                finish();
-            }
-
-
-
-        }
-    }*/
 }
